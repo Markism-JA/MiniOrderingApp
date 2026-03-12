d08570d5d984e7c1586b6754e44867500152d6b9
marky <markism1224amarille@gmail.com>
2026-03-12 20:48:41 +0800

    feat: add ons add default text

--- lib/model/add_ons.dart
+++ lib/model/add_ons.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
 enum AddOns {
-  candles(LucideIcons.flame),
-  sparklers(LucideIcons.sparkles),
-  birthdayTopper(LucideIcons.star);
+  candles(LucideIcons.flame, "Candles"),
+  sparklers(LucideIcons.sparkles, "Sparklers"),
+  birthdayTopper(LucideIcons.star, "Birthday Topper");
 
   final IconData icon;
+  final String label;
 
-  const AddOns(this.icon);
+  const AddOns(this.icon, this.label);
 }
