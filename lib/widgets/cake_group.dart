2dd9cb683e9f42f6af99790ef7cca8d31795d0e1
marky <markism1224amarille@gmail.com>
2026-03-12 16:19:47 +0800

    feat: cake card grouping accepts a list of cake model

---
+++ lib/widgets/cake_group.dart
@@ -0,0 +1,50 @@
+import 'package:advmobprog_midterms_tp03_amarille/model/Cake.dart';
+import 'package:flutter/material.dart';
+import 'package:advmobprog_midterms_tp03_amarille/widgets/cake_card.dart';
+
+class CakeGroup extends StatefulWidget {
+  final List<Cake> cakes;
+  final ValueChanged<Cake?> onSelectionChanged;
+
+  const CakeGroup({
+    super.key,
+    required this.cakes,
+    required this.onSelectionChanged,
+  });
+
+  @override
+  State<CakeGroup> createState() => _CakeGroupState();
+}
+
+class _CakeGroupState extends State<CakeGroup> {
+  String? _selectedCakeId;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 0.85,
+      ),
+      itemCount: widget.cakes.length,
+      itemBuilder: (context, index) {
+        final cake = widget.cakes[index];
+
+        return CakeCard(
+          cake: cake,
+          selectedCakeId: _selectedCakeId,
+          onSelected: (id) {
+            setState(() {
+              _selectedCakeId = id;
+            });
+            widget.onSelectionChanged(cake);
+          },
+        );
+      },
+    );
+  }
+}
